70df753755cb61fad3c6c070bcce70dd289857ec
Daniel Ye <danielye@Daniels-Laptop.local>
2023-10-01 23:53:53 -0400

    ability to place items on grid

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 // module pattern -> 1 of something
 const gameBoard = (() => {
-  const board = [];
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
 
   // gives only read access to board
   const getBoard = () => board;
 
+  const updateBoard = (piece, row, col) => {
+    board[row][col] = piece;
+  };
+
   return {
     getBoard,
+    updateBoard,
   };
 })();
 
 // factory function -> many of something
-const playerFactory = () => {
-  return {};
+const playerFactory = (piece) => {
+  return { piece };
 };
 
-const gameController = (() => {
-  return {};
-})();
-
 const displayController = (() => {
-  return {};
+  const boardItems = document.querySelectorAll(".board-item");
+
+  const clickHandlerItem = (e) => {
+    gameBoard.updateBoard("X", e.target.dataset.row, e.target.dataset.col);
+    e.target.innerText = "X";
+    console.log(gameBoard.getBoard());
+  };
+  boardItems.forEach((item) =>
+    item.addEventListener("click", clickHandlerItem)
+  );
 })();
